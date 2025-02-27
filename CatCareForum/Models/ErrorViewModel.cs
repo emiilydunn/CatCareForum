c98ac8892c56b92aa90adb6a3ac0736f97de1a9b
Emily Dunn <emilydunn13@gmail.com>
2025-02-27 11:23:06 -0400

    Add project files.

---
+++ CatCareForum/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CatCareForum.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
